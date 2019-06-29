bbbd2ab52ac066ad32f439afdd8ffa42754310ea
Magno Carvalho <carvalhodemagno@gmail.com>
2019-06-29 17:27:05 -0400

    Changes to be committed: 	modified:   3_Arquitetura/Arquitetura.tex 	modified:   4_Implementacao/Implementacao.tex 	modified:   5_Resultados/Resultados.tex 	modified:   7_Apendice/apendice.tex 	new file:   figuras/erro_corrente-eps-converted-to.pdf 	new file:   figuras/erro_corrente.eps 	new file:   figuras/erro_frequencia-eps-converted-to.pdf 	new file:   figuras/erro_frequencia.eps 	new file:   figuras/erro_potencia-eps-converted-to.pdf 	new file:   figuras/erro_potencia.eps 	new file:   figuras/erro_tensao-eps-converted-to.pdf 	new file:   figuras/erro_tensao.eps 	new file:   figuras/magno/NewCallback.m 	new file:   figuras/magno/erro.txt 	modified:   figuras/magno/magno.m 	renamed:    figuras/magno/magno.txt -> figuras/magno/medidas.txt 	modified:   figuras/plot_corrente-eps-converted-to.pdf 	modified:   figuras/plot_corrente.eps 	modified:   figuras/plot_frequencia-eps-converted-to.pdf 	modified:   figuras/plot_frequencia.eps 	modified:   figuras/plot_potencia-eps-converted-to.pdf 	modified:   figuras/plot_potencia.eps 	modified:   figuras/plot_tensao-eps-converted-to.pdf 	modified:   figuras/plot_tensao.eps 	modified:   tcc_magno.pdf

--- figuras/magno/magno.m
+++ figuras/magno/magno.m
@@ -1,10 +1,37 @@
-Base = load('magno.txt');
+Base = load('erro.txt');
 j = 1;
 for i = 1 : 4
     figure((j+1)/2);
     plot(Base(:,9),Base(:,j),'b','LineWidth',2.5);
+    set(gca,'FontSize',14)
+    grid on
     hold on
     plot(Base(:,9),Base(:,j+1),'r','LineWidth',2.5);
+    set(gca,'FontSize',14)
+    grid on
     j = j + 2;
-    legend('Medição da placa','Medição do alicate')
+   % if i == 1
+   %         legend('Medições do protótipo [A]','Medições do alicate [A]')
+   % elseif i == 2
+   %         legend('Medições do protótipo [V]','Medições do alicate [V]')
+   % elseif i == 3
+   %         legend('Medições do protótipo [Hz]','Medições do alicate [Hz]')
+   % elseif i == 4
+   %         legend('Medições do protótipo [W]','Medições do alicate [W]')
+   % end
+   if i == 1
+           legend('Erro relativo [%]','Erro absoluto [A]') 
+   elseif i == 2
+           legend('Erro relativo [%]','Erro absoluto [V]')
+   elseif i == 3
+           legend('Erro relativo [%]','Erro absoluto [Hz]')
+   elseif i == 4
+           legend('Erro relativo [%]','Erro absoluto [W]')
+   end
+   
+    alldatacursors = findall(gcf,'type','hggroup')
+    set(alldatacursors,'FontSize',16)
+    set(alldatacursors,'FontName','Times')
+    set(alldatacursors, 'FontWeight', 'bold')
+   
 end
